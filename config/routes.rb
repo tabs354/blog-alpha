3df27f73232c6efce308e64fd6d82e476d6568ad
Jasper <tabacugjasper@gmail.com>
2019-08-07 13:26:41 +0800

    Sign Up for Users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except:[:new]
   resources :articles
 
 end
