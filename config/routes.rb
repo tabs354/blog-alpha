a858aa7b6591baed84b89c5c08efbf825c47a8d3
Jasper <tabacugjasper@gmail.com>
2019-08-07 19:26:59 +0800

    Part of Login

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   resources :users, except:[:new]
   resources :articles
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
