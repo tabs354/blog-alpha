1dffc4e5297b2e3497c5d9e13222791fcba22434
Jasper <tabacugjasper@gmail.com>
2019-08-07 19:29:17 +0800

    Part of Login 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,20 @@ class SessionsController <ApplicationController
   end
 
   def create
-    render "new"
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      flash[:success] = "You have successfully logged in"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information!"
+      render 'new'
+    end
   end
 
   def destroy
-
+    session[:user_id] = nil
+    flash[:success] = "You have successfully logged out"
+    redirect_to root_path
   end
 end
